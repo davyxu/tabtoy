b9726a5086c1599d80ed8e2673081e8042c47406
davyxu <sunicdavy@qq.com>
2020-06-10 11:15:15 +0800

    更新生成shell

--- Make.sh
+++ Make.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
-export GOPROXY=https://goproxy.io
+#export GOPROXY=https://goproxy.io
+
+if [ "$1" == "" ] 
+then
+	echo "Usage: Make.sh version"
+	exit 1
+fi
+
+Version=$1
+
+export GOARCH=amd64
+export GOOS=windows
 
 go build -v -o ./tabtoy.exe github.com/davyxu/tabtoy
+
+tar zcvf tabtoy-${Version}-win64.tar.gz ./tabtoy.exe
+
+export GOARCH=amd64
+export GOOS=linux
+
+go build -v -o ./tabtoy github.com/davyxu/tabtoy
+	
+tar zcvf tabtoy-${Version}-linux-x86_64.tar.gz ./tabtoy
+
+export GOARCH=amd64
+export GOOS=darwin
+
+go build -v -o ./tabtoy github.com/davyxu/tabtoy
+	
+tar zcvf tabtoy-${Version}-osx-x86_64.tar.gz ./tabtoy
