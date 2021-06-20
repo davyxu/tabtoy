0a8c7cb0725211d5d56569e140a9c294c797e910
davyxu <sunicdavy@qq.com>
2021-06-20 20:16:45 +0800

    更新build shell

--- Make.sh
+++ Make.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 Version=3.1.2
 
-export GOARCH=amd64
 BuildSourcePackage="github.com/davyxu/tabtoy/build"
+BinaryPackage="github.com/davyxu/tabtoy"
+BinaryName="tabtoy"
 
 BuildBinary()
 {
@@ -14,15 +15,19 @@ BuildBinary()
   GitCommit=$(git rev-parse HEAD)
   VersionString="-X \"${BuildSourcePackage}.BuildTime=${BuildTime}\" -X \"${BuildSourcePackage}.Version=${Version}\" -X \"${BuildSourcePackage}.GitCommit=${GitCommit}\""
 
-  go build -v -p 4 -o "${TargetDir}"/tabtoy -ldflags "${VersionString}" github.com/davyxu/tabtoy
+  go build -v -p 4 -o "${TargetDir}"/${BinaryName} -ldflags "${VersionString}" ${BinaryPackage}
   PackageDir=$(pwd)
   cd "${TargetDir}"
-  tar zcvf "${PackageDir}"/tabtoy-${Version}-"${1}"-x86_64.tar.gz tabtoy
+  tar zcvf "${PackageDir}"/${BinaryName}-${Version}-"${1}"-x86_64.tar.gz ${BinaryName}
   cd "${PackageDir}"
 }
 
 
+if [[ ${1} == "" ]]; then
+  BuildBinary windows
+  BuildBinary linux
+  BuildBinary darwin
+else
+  BuildBinary "${1}"
+fi
 
-BuildBinary windows
-BuildBinary linux
-BuildBinary darwin
